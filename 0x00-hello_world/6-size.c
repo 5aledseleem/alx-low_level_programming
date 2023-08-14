8fc1b4469a729ab767edd25d2d7e5889e3b45836
khaled seleem <khaledseleem446@gmail.com>
2023-08-13 19:42:08 -0700

    elgendy

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriptin: A C program that prints the size of various types using sizeof function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of a int: %lubyte(s)\n", sizeof(int));
+	printf("size of a long int: %lubyte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lubyte(s)\n", sizeof(long long int));
+	printf("size of a float: %lubyte(s)\n", sizeof(float));
+	return (0);
+}
