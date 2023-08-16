3848c5528e6ea8ca58bcf08307fa09480cfcca7b
khaled seleem <khaledseleem446@gmail.com>
2023-08-16 13:00:00 -0700

    elgendey

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,15 +13,16 @@
 void print_remaining_days(int month, int day, int year)
 {
 	/**
-	 * leep year when it can be divided
-	 * by 100 and 400 or by 4 evenly
+	* leep year when it can be divided
+	* by 100 and 400 or by 4 evenly
 	*/
+
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
-			day++;
-		}
+		day++;
+	}
 
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
@@ -30,12 +31,12 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
