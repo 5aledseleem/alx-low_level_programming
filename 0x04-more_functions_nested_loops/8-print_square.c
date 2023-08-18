cb1a1edaeefad3f3ac58e75dc4b8d7993524d8c2
khaled seleem <khaledseleem446@gmail.com>
2023-08-18 13:47:55 -0700

    elgendy

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,9 @@ void print_square(int size)
 {
 	int row, colum;
 
-	for (row = 1; row <= size; row++)
+	for (row = 0; row < size; row++)
 	{
-		for (colum = 1; colum <= size; colum++)
+		for (colum = 0; colum < size; colum++)
 			_putchar('#');
 		_putchar('\n');
 	}
