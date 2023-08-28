3ad27fe0ba55f3d1e92a2fdb38c8bfe756df284a
khaled seleem <khaledseleem446@gmail.com>
2023-08-28 07:03:27 -0700

    elgendey

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,12 @@
  * Return: pointer s
 */
 
-char *_memset(char *s, charb, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; n > 0; n--)
-	s[a] = b ;
+	s[a] = b;
 
 	return (s);
+}
